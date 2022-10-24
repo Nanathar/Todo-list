bfc4237035c5813f7520d9ea1aaaa28269301a69
nanathar <lewalskimateusz94@gmail.com>
2022-10-24 19:50:21 +0200

    Add new project

--- main.js
+++ main.js
@@ -5,27 +5,43 @@ const inputSearch = document.querySelector('input.input-search')
 const span = document.querySelector('h3 span');
 const taskList = [];
 
-
+const deleteTask = (e) => {
+  e.preventDefault();
+  const index = e.target.parentNode.dataset.key;
+  taskList.splice(index, 1);
+  renderList();
+  span.textContent = taskList.length;
+}
 
 const addTask = (e) => {
   e.preventDefault();
   const task = inputTask.value;
   if (task === '') return alert('Dear sir/madam add a task !!!');
   const newTask = document.createElement('input');
-  const btnEdit = document.createElement('button');
-  const btnDelete = document.createElement('button');
-  btnEdit.textContent = 'Edit';
-  btnDelete.textContent = 'Delete';
   newTask.className = 'input-task';
   newTask.value = task;
   newTask.readOnly = "readonly";
-  formSection.append(newTask, btnEdit, btnDelete);
-
-
-
-
-
+  const btnEdit = document.createElement('button');
+  btnEdit.textContent = 'Edit';
+  btnEdit.className = 'btn-edit';
+  const btnDelete = document.createElement('button');
+  btnDelete.textContent = 'Delete';
+  btnDelete.className = 'btn-delete';
+  const div = document.createElement('div');
+  div.append(newTask, btnEdit, btnDelete);
+  taskList.push(div);
+  renderList();
+  div.querySelector('.btn-delete').addEventListener('click', deleteTask);
+  span.textContent = taskList.length;
   inputTask.value = '';
+}
+
+const renderList = () => {
+  formSection.textContent = '';
+  taskList.forEach((task, i) => {
+    task.dataset.key = i;
+    formSection.appendChild(task);
+  })
 }
 
 formHeader.addEventListener('submit', addTask)
