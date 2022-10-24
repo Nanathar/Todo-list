f1a5b063bf538f93f9ef1a1fe71a2d1dfeec0fee
nanathar <lewalskimateusz94@gmail.com>
2022-10-24 20:39:56 +0200

    Add new project

--- main.js
+++ main.js
@@ -3,14 +3,36 @@ const formSection = document.querySelector('form.section-form');
 const inputTask = document.querySelector('input.input-new-task');
 const inputSearch = document.querySelector('input.input-search')
 const span = document.querySelector('h3 span');
+
 const taskList = [];
 
-const deleteTask = (e) => {
+const removeTask = (e) => {
   e.preventDefault();
   const index = e.target.parentNode.dataset.key;
   taskList.splice(index, 1);
   renderList();
   span.textContent = taskList.length;
+}
+
+const editTask = (e) => {
+  e.preventDefault();
+  const btn = e.target
+  const style = () => {
+    btn.style.backgroundClip = 'text';
+    btn.style.webkitBackgroundClip = 'text';
+    btn.style.webkitTextFillColor = 'transparent';
+  }
+
+  if (btn.textContent === 'Edit') {
+    btn.textContent = 'Save';
+    btn.style.background = '-webkit-linear-gradient(rgba(238, 238, 238, 0.8078431373), rgba(26, 13, 218, 0.7882352941))';
+    style();
+  } else if(btn.textContent === 'Save') {
+    btn.textContent = 'Edit';
+    btn.style.background = '-webkit-linear-gradient(rgba(238, 238, 238, 0.8078431373), rgb(79, 78, 82))';
+    style();
+  }
+
 }
 
 const addTask = (e) => {
@@ -31,7 +53,8 @@ const addTask = (e) => {
   div.append(newTask, btnEdit, btnDelete);
   taskList.push(div);
   renderList();
-  div.querySelector('.btn-delete').addEventListener('click', deleteTask);
+  div.querySelector('.btn-delete').addEventListener('click', removeTask);
+  div.querySelector('.btn-edit').addEventListener('click', editTask);
   span.textContent = taskList.length;
   inputTask.value = '';
 }
